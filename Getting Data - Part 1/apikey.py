50758f850c5845c6799109271cb73feee1ad5eda
deepa-kakade-git <deep_303@yahoo.com>
2024-04-15 16:35:32 -0400

    working on aws

---
+++ Getting Data - Part 1/apikey.py
@@ -0,0 +1,33 @@
+{
+ "cells": [
+  {
+   "cell_type": "raw",
+   "id": "353cd8b8-2767-4857-b56b-0c6ebb1ab9c3",
+   "metadata": {},
+   "source": [
+    "api key = de5ca03963df7993efe52bad5dbfeca6"
+   ]
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3 (ipykernel)",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.12.2"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
